295058f04eee47537bba6dcce500508b5bf66297
pizzaburgare <emil.scharin@gmail.com>
2023-12-01 14:11:32 +0100

    Updated all to switch case

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = new Client({
   ],
 });
 const prefix = "meister";
+
 const adminRoleID = process.env.ADMIN_ROLE_ID
 
 client.once("ready", () => {
@@ -52,85 +53,87 @@ client.on("messageCreate", (message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    // Help
-    if (command == "" || command == "help"){
+    switch (command) {
+      // Help
+      case "help":
+      case "":
       message.channel.send(helpMsg)
-    }
-    if (command == "testrole"){
-      const userName = message.member.user.globalName
-      if (hasRoleId(message,adminRoleID)){
-        message.channel.send("User " + userName + " is allowed")
-      }else{
-        message.channel.send("User " +userName + " is NOT allowed")
-      }
-    }
-    // Check the command
-    if (command === "new") {
-      const subCommand = args.shift().toLowerCase();
-      switch (subCommand) {
-        case "ctf":
-          // Permission check
-          if (!hasRoleId(message,adminRoleID)){
-            break;
-          }
+        break;
+      
+      case "testrole":
+        const userName = message.member.user.globalName
+        if (hasRoleId(message,adminRoleID)){
+          message.channel.send("User " + userName + " is allowed")
+        }else{
+          message.channel.send("User " +userName + " is NOT allowed")
+        }
+        break;
+      case "new":
+        const subCommand = args.shift().toLowerCase();
+        switch (subCommand) {
+          case "ctf":
+            // Permission check
+            if (!hasRoleId(message,adminRoleID)){
+              break;
+            }
 
-          // Create a new category
-          const categoryName = args.join(" ");
-          message.guild.channels
-            .create({
-              name: categoryName,
-              type: ChannelType.GuildCategory,
-            })
-            .then((e) => {
-              message.guild.channels.create({
+            // Create a new category
+            const categoryName = args.join(" ");
+            message.guild.channels
+              .create({
                 name: categoryName,
+                type: ChannelType.GuildCategory,
+              })
+              .then((e) => {
+                message.guild.channels.create({
+                  name: categoryName,
+                  type: ChannelType.GuildText,
+                  parent: e.id,
+                });
+                message.guild.channels.create({
+                  name: categoryName,
+                  type: ChannelType.GuildVoice,
+                  parent: e.id,
+                });
+              });
+            message.channel.send(`New CTF '${categoryName}' created.`);
+          break;
+          case "chall":
+            // Create a new channel inside the specified category
+            const ctfName = args.shift().toLowerCase();
+            const challName = args.join(" ");
+            const category = findCategoryByName(message.guild,ctfName);
+
+            if (category && category.type === ChannelType.GuildCategory && category.name != "general") {
+              message.guild.channels.create({
+                name: challName,
                 type: ChannelType.GuildText,
-                parent: e.id,
+                parent: category,
               });
-              message.guild.channels.create({
-                name: categoryName,
-                type: ChannelType.GuildVoice,
-                parent: e.id,
-              });
-            });
-          message.channel.send(`New CTF '${categoryName}' created.`);
-          break;
-        case "chall":
-          // Create a new channel inside the specified category
-          const ctfName = args.shift().toLowerCase();
-          const challName = args.join(" ");
-
-          const category = findCategoryByName(message.guild,ctfName);
-
-          if (category && category.type === ChannelType.GuildCategory && category.name != "general") {
-            message.guild.channels.create({
-              name: challName,
-              type: ChannelType.GuildText,
-              parent: category,
-            });
-            message.channel.send(
-              `New channel '${challName}' created inside '${category.name}'.`
-            );
-          } else {
-            message.channel.send("Make sure ctf category exists");
-            message.channel.send("Usage: meister new chall <CTF_NAME> <CHALL_NAME>");
-          }
-
-          break;
+              message.channel.send(
+                `New channel '${challName}' created inside '${category.name}'.`
+              );
+            } else {
+              message.channel.send("Make sure ctf category exists");
+              message.channel.send("Usage: meister new chall <CTF_NAME> <CHALL_NAME>");
+            }
         default:
-          console.log(subCommand.length)
           break;
       }
-    }
-          
-    if (command === "solved") {
-      // Change the channel name to include a checkmark
-      const solvedString = args.join(" ");
-      const newChannelName = `${message.channel.name} ✅`;
-      message.channel.setName(newChannelName);
-      message.channel.send(`Challenge solved: \`${solvedString}\``);
+      case "solved":
+        const parent = message.channel.parent
+        if (parent.name != "general"){        
+          const newChannelName = `${message.channel.name} ✅`;
+          message.channel.setName(newChannelName);
+          message.channel.send(`Challenge solved: \`${message.channel.name}\``);
+        }
+        break;
+      default:
+        message.channel.send("Command not found: "+ command)
+        break;
     }
   }
+
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN).then((e) => console.log(e));
