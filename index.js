0186cc3a51133486aa870d855c20891b801e66b6
pizzaburgare <emil.scharin@gmail.com>
2023-12-01 13:42:14 +0100

    Ändrat till en switch-case för subcategory
    Possible solution for not creating in general

--- index.js
+++ index.js
@@ -21,15 +21,25 @@ Testing
   meister testrole
 Creating new ctfs:
   meister new ctf <CTF-NAME>
-  mesiter new chall <CHALL-NAME>
+  mesiter new chall <CTF-NAME> <CHALL-NAME>
 `
 const hasRoleId = (message,id)=>{
   const member = message.member;
   if (member.roles.cache.some(role => role.id === id)) {
     return true
   }else{
+    const userName = message.member.user.globalName
+    message.channel.send("User " +userName + " invalid permissions")
     return false
   }
+}
+
+function findCategoryByName(guild, categoryName) {
+  // Filter the channels to find the category with the given name
+  const category = guild.channels.cache.find(channel => 
+    channel.type==ChannelType.GuildCategory && channel.name.toLowerCase() == categoryName
+  );
+  return category; // This will be undefined if no category is found
 }
 
 client.on("messageCreate", (message) => {
@@ -55,47 +65,65 @@ client.on("messageCreate", (message) => {
       }
     }
     // Check the command
-    if (command === "new" && hasRoleId(message,adminRoleID)) {
+    if (command === "new") {
       const subCommand = args.shift().toLowerCase();
-      if (subCommand === "ctf") {
-        // Create a new category
-        const categoryName = args.join(" ");
-        message.guild.channels
-          .create({
-            name: categoryName,
-            type: ChannelType.GuildCategory,
-          })
-          .then((e) => {
-            message.guild.channels.create({
+      switch (subCommand) {
+        case "ctf":
+          // Permission check
+          if (!hasRoleId(message,adminRoleID)){
+            break;
+          }
+
+          // Create a new category
+          const categoryName = args.join(" ");
+          message.guild.channels
+            .create({
               name: categoryName,
+              type: ChannelType.GuildCategory,
+            })
+            .then((e) => {
+              message.guild.channels.create({
+                name: categoryName,
+                type: ChannelType.GuildText,
+                parent: e.id,
+              });
+              message.guild.channels.create({
+                name: categoryName,
+                type: ChannelType.GuildVoice,
+                parent: e.id,
+              });
+            });
+          message.channel.send(`New CTF '${categoryName}' created.`);
+          break;
+        case "chall":
+          // Create a new channel inside the specified category
+          const ctfName = args.shift().toLowerCase();
+          const challName = args.join(" ");
+
+          const category = findCategoryByName(message.guild,ctfName);
+
+          if (category && category.type === ChannelType.GuildCategory && category.name != "general") {
+            message.guild.channels.create({
+              name: challName,
               type: ChannelType.GuildText,
-              parent: e.id,
+              parent: category,
             });
-            message.guild.channels.create({
-              name: categoryName,
-              type: ChannelType.GuildVoice,
-              parent: e.id,
-            });
-          });
-        message.channel.send(`New CTF '${categoryName}' created.`);
-      } else if (subCommand === "chall" && hasRoleId(message,adminRoleID)) {
-        // Create a new channel inside the specified category
-        const channelName = args.join(" ");
-        const category = message.channel.parent;
-        if (category && category.type === ChannelType.GuildCategory) {
-          message.guild.channels.create({
-            name: channelName,
-            type: ChannelType.GuildText,
-            parent: category,
-          });
-          message.channel.send(
-            `New channel '${channelName}' created inside '${category.name}'.`
-          );
-        } else {
-          message.channel.send("Please use this command inside a category.");
-        }
+            message.channel.send(
+              `New channel '${challName}' created inside '${category.name}'.`
+            );
+          } else {
+            message.channel.send("Make sure ctf category exists");
+            message.channel.send("Usage: meister new chall <CTF_NAME> <CHALL_NAME>");
+          }
+
+          break;
+        default:
+          console.log(subCommand.length)
+          break;
       }
-    } else if (command === "solved") {
+    }
+          
+    if (command === "solved") {
       // Change the channel name to include a checkmark
       const solvedString = args.join(" ");
       const newChannelName = `${message.channel.name} ✅`;
